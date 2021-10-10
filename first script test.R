d2796915b3a3d3d6d27cc6d3eb70cf428dc211f5
evatvan <evelynyijiazhang@gmail.com>
2021-10-09 22:13:34 -0700

    update script after t-step a

--- first script test.R
+++ first script test.R
@@ -1,2 +1,3 @@
-print("This file was created within RStudio")
-print("And now it lives on GitHub")
+print("This file was created within RStudio for a second time")
+print("And now it lives on GitHub well hopefully")
+print("Well since it says no new line. Here it is.")
